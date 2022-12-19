11ff179a026681000f23012e555c40cf19030591
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2022-12-19 22:28:25 +0800

    update todo

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -50,13 +50,85 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index){
-          return ListTile(
-            leading: Text('${index + 1}'),
-            title: Text(items[index]['title']),
-            subtitle: Text(items[index]['body']),
+          final item = items[index] as Map;
+          return Dismissible(
+              key: ValueKey(item[index]),
+              background: slideLeftBackground(),
+              confirmDismiss: (direction) async{
+                if (direction == DismissDirection.endToStart) {
+                  final bool res = await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          content: Text(
+                              "Are you sure you want to delete\n${items[index]['title']}?"),
+                          actions: <Widget>[
+                            FloatingActionButton(
+                              child: const Text(
+                                "Cancel",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            FloatingActionButton(
+                              child: const Text(
+                                "Delete",
+                                style: TextStyle(color: Colors.red),
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  // items.removeAt(index);
+                                  deleteTodos('_id');
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      });
+                  return res;
+                } else {
+                }
+                return null;
+              },
+              child: ListTile(
+                leading: Text('${index + 1}'),
+                title: Text(items[index]['title']),
+                subtitle: Text(items[index]['body']),
+              ),
           );
         },
       ),
+    );
+  }
+  Widget slideLeftBackground(){
+    return Container(
+      color: Colors.red,
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const <Widget>[
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            Text(
+              " Delete",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.right,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        )
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_handsonsemifinal/homepage.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -7,6 +8,7 @@ void main(){
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
+    home: const HomePage()
   ));
 }
 
